a60ba9de49dc629a7b2e1919a627943fbc253031
kezunlong <13761596352@163.com>
2015-04-25 21:50:15 +0800

    xDirectory - Copy Files and SubFolders.

---
+++ Lifepoem.Foundation.Utilities/IO/xDirectoryStatus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IR.API.Foundation.Utilities.IO
+{
+    /// <summary>
+    /// The Various levels of Status for the xDirectory 'StartCopy' method.
+    /// </summary>
+    public enum xDirectoryStatus
+    {
+        /// <summary>
+        /// The xDirectory Copy Thread is Stopped.
+        /// </summary>
+        Stopped,
+
+        /// <summary>
+        /// The xDirectory Copy Thread is Starting.
+        /// </summary>
+        Started,
+
+        /// <summary>
+        /// The xDirectory Copy Thread is Indexing.
+        /// </summary>
+        Indexing,
+
+        /// <summary>
+        /// The xDirectory Copy Thread is Copying Folders.
+        /// </summary>
+        CopyingFolders,
+
+        /// <summary>
+        /// The xDirectory Copy Thread is Copying Files.
+        /// </summary>
+        CopyingFiles
+    };
+}
